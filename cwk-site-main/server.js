8d3622a6599fa18925df52938d3fb9b9ed65d985
dillemmageist <87817116+scriptefer@users.noreply.github.com>
2025-05-21 18:05:19 +0300

    Update server.js

--- cwk-site-main/server.js
+++ cwk-site-main/server.js
@@ -97,7 +97,7 @@ const requireAuth = (req, res, next) => {
     if (req.session && req.session.isAuthenticated) {
         next();
     } else {
-        res.status(401).json({ error: 'Unauthorized' });
+       res.redirect('/admin/login');
     }
 };
 
